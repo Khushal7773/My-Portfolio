34c472f6d561b45c9a4f2ab1b732d5fa438765a1
Khushal7773 <khushalsinghpanwar07@gmail.com>
2023-08-04 17:58:36 +0530

    About page is completed

---
+++ src/about/SubheadingsData.js
@@ -0,0 +1,60 @@
+const SubheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                <p>
+                    I am a passioante Front End Developer.
+                </p>
+                </>
+            )
+        }
+    ],
+    2: [
+        {
+            title: "2020 B.Tech Degree",
+            content: (
+                <>
+                <p>
+                    I passed out from Maulana Azad National Institute of Technology(MANIT), Bhopal with the B. Tech Degree in Civil Engineering.
+                </p>
+                </>
+            )
+        },
+        {
+            title: "2015 High School",
+            content: (
+                <>
+                <p>
+                    I completed my schooling from Sardar Patel Sr. Sec. School, acquiring 92% in 12th standard.
+                </p>
+                </>
+            )
+        },
+    ],
+    3: [
+        {
+            title: "2023 Front End Developer",
+            content: (
+                <>
+                <p>
+                    I learnt assortment of technologies like React, Redux, Tailwind, Javascript etc. And built many amazing personal projects. Link for the same are available at Projects section of this website.
+                </p>
+                </>
+            )
+        },
+        {
+            title: "2023 Personal Goal Pursuit",
+            content: (
+                <>
+                <p>
+                    After completing my graduation, I started preparing for UPSC-Civil Services Examination. This journey made me a critical thinker.
+                </p>
+                </>
+            )
+        },
+    ],
+}
+
+export default SubheadingsData;
---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css"
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>KHUSHAL LEVEL 31</h1>
+            <div id="playerStats_lines">
+            <span id="playerStats_lines_thick"></span>
+            <span id="playerStats_lines_thin"></span>
+            </div>
+            <h2>Frontend Developer</h2>
+        </div>
+    );
+};
+
+export default PlayerStats;
